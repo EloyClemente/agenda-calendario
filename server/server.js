b4696a8842413b8b72a7b8515d677890ea29ea1a
Eloy Clemente <eloyclemente1@gmail.com>
2018-01-01 19:32:15 +0100

    mongo

--- server/server.js
+++ server/server.js
@@ -8,9 +8,29 @@ const app = express()
 
 app.use(express.static('public'))
 //........................................
+const mongoose = require('mongoose')
+//........................................
+
+
+
+
+
+
+
+
 
 
 
 var port = process.env.PORT || 3000
+var url  = process.env.MONGODB_URI || 'mongodb://localhost:27017/database-04'
 
-app.listen(port, () => { console.log('Ejecutando en 3000') })
+//....................................................................
+mongoose.connect(url, (err, res) =>
+{
+	if (err) throw err
+	console.log('')
+	console.log('- Conectado a mongo')
+
+	server.listen(port, () => {console.log('- Ejecutando en 3000')})
+})
+//....................................................................

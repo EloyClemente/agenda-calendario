5f85b44b7276f1578809df904b7f040b1a5f7eeb
Eloy Clemente <eloyclemente1@gmail.com>
2018-01-01 19:35:37 +0100

    Mongo

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ const mongoose = require('mongoose')
 
 
 var port = process.env.PORT || 3000
-var url  = process.env.MONGODB_URI || 'mongodb://localhost:27017/database-04'
+//var url  = process.env.MONGODB_URI || 
 
 //....................................................................
-mongoose.connect(url, (err, res) =>
+mongoose.connect('mongodb://localhost:27017/database-04', (err, res) =>
 {
 	if (err) throw err
 	console.log('')
